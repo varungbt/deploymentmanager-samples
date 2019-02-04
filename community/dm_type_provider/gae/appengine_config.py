7eda8ae64054b999a33b2c83cee33d94f9d8c55e
Varunkumar Manohar <VManohar@slb.com>
2019-02-04 14:57:52 -0600

    remove auth and fix id issues in put request

--- community/dm_type_provider/gae/appengine_config.py
+++ community/dm_type_provider/gae/appengine_config.py
@@ -6,4 +6,4 @@ from google.appengine.ext import vendor
 vendor.add('lib')
 sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
